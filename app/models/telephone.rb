2fc3bb151fc3a5949e2a29c843bc472674bf363f
Fabio Saito <fabio.h.saito@gmail.com>
2022-02-11 16:08:39 -0300

    rails-challenge

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -1,7 +1,19 @@
+class UniqueMainPhone < ActiveModel::Validator
+  def validate(record)
+    first_contact_telephone = Telephone.where(contact_id: record.contact_id).count.zero?
+    if record.principal == true && !first_contact_telephone
+      Telephone.where(principal: true, contact_id: record.contact_id).first.update(principal: false)
+    end
+  end
+end
+
 class Telephone < ApplicationRecord
   belongs_to :contact
 
   validates :category, presence: true
-  validates :principal, inclusion: [true, false], uniqueness: { scope: :contact_id}
+  validates :principal, inclusion: [true, false]
   validates :number, presence: true, uniqueness: { scope: :contact_id}
+
+  validates_with UniqueMainPhone
 end
+
--- spec/models/telephone_spec.rb
+++ spec/models/telephone_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Telephone, type: :model do
-  # inclusion of
+  fixtures :telephones
+  
+  describe "keep unique principal phone" do
+    it "should change previous principal phone to false" do
+      
+      tel_new = {
+        number: "8888-8888",
+        category: "home",
+        principal: true}
 
-  # context "create new" do
-  #   it "should not increment a new telephone to table" do
-  #      expect(Telephone.new.save).to be false
-  #   end
+      Contact.first.telephones.create(tel_new)
 
-  #   it "should increment a new telephone to table" do
-  #     User.create
-  #     User.first.contacts.create
-  #     expect( Contact.first.telephones.new.save).to be true
-  #   end
-  # end
+      Telephone1 = Telephone.find(telephones(:tel1).id)
 
+      expect(Telephone1.principal).to be false
+    end
+  end
 end
