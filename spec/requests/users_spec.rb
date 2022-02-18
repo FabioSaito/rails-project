25a9f80c653e74e0bebd23ad8c3226e2c3b96f13
Fabio Saito <fabio.h.saito@gmail.com>
2022-02-18 17:16:20 -0300

    include tests and fix blank space

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,40 +18,40 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  # describe "GET /new" do
-  #   it "returns http success" do
-  #     get "/users/new"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /new" do
+    it "returns http success" do
+      get "/users/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "POST /create" do
-  #   it "returns http success" do
-  #     post "/users/create"
+  describe "POST /create" do
+    it "returns http success" do
+      post "/users/create"
       
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET /edit" do
-  #   it "returns http success" do
-  #     get "/users/edit"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/users/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "PUT /update" do
-  #   it "returns http success" do
-  #     put "/users/update"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "PUT /update" do
+    it "returns http success" do
+      put "/users/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET /destroy" do
-  #   it "returns http success" do
-  #     get "/users/destroy"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/users/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
